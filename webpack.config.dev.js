2d63b722dd54ee1ca14099892f8e1c672e6cbfdf
James Conkling <james.lane.conkling@gmail.com>
2017-10-11 16:57:12 -0400

    babel compile build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,6 +22,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
         test: /\.css$/,
         use: [
           { loader: "style-loader" },
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,6 +18,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
         test: /\.css$/,
         use: [
           { loader: "style-loader" },
@@ -31,6 +36,12 @@ module.exports = {
     new HtmlWebpackPlugin({ template: './index.html', inject: 'body' }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      mangle: false
     })
   ],
 
