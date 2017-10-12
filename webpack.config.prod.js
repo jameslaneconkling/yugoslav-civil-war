caaddf29200eb832ace98985447053b1d55fd74b
James Conkling <james.lane.conkling@gmail.com>
2017-10-11 20:42:01 -0400

    fix minification

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -37,12 +37,13 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false
-    //   },
-    //   mangle: false
-    // })
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        comparisons: false
+      },
+      mangle: false
+    })
   ],
 
   devtool: 'source-map'
